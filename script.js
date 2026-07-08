a50efd55e583af932abcb46bbe8040eff256cde0
WhereisKF <fmb659@gmail.com>
2026-07-08 03:12:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ function clearDisplay() {
   display.value = "";
 }
 
+function backspace() {
+    display.value = display.value.slice(0, -1);
+}
 function calculate() {
   try {
     let correctAnswer = eval(display.value);
